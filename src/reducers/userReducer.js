23db8a587f032bb6560c9d0c9275e722bec26718
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-07-22 16:27:53 +0200

    add user reducer to store user info

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,19 @@
+import { userActions } from "../constants";
+
+const initialState = {
+  user: null,
+  error: null
+}
+
+export default function(state = initialState, action) {
+  const { type } = action;
+  switch(type) {
+    case userActions.FETCH_USER_DATA_SUCCESS:
+      const { user } = action;
+      return { user, error: null }
+    case userActions.FETCH_USER_DATA_ERRORS:
+      return Object.assign({}, state, { error: action.message });
+    default:
+      return state;
+  }
+};
