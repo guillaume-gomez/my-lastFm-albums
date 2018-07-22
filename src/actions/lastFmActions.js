2c2bda323a953ace3ee7804c12812be93dc5c6c4
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-07-22 16:29:55 +0200

    add new actions

--- src/actions/lastFmActions.js
+++ src/actions/lastFmActions.js
@@ -1,21 +1,46 @@
-import { lastFmActions } from "../constants";
+import { lastFmActions, userActions } from "../constants";
 
-export function lasfmQuery() {
+export function lasfmQuery(from = null, to = null) {
   return {
-    type: lastFmActions.FETCH_LAST_FM
+    type: lastFmActions.FETCH_LAST_FM,
+    from,
+    to
   };
 };
 
 export function lastFmResult(data) {
   return {
     type: lastFmActions.FETCH_LAST_FM_SUCCESS,
-    payload: data.weeklyalbumchart.album
+    payload: data.weeklyalbumchart.album,
+    from: data.weeklyalbumchart["@attr"].from,
+    to: data.weeklyalbumchart["@attr"].to
   };
 }
 
 export function lastFmError(message) {
   return {
     type: lastFmActions.FETCH_LAST_FM_ERRORS,
+    message
+  };
+}
+
+export function fetchUser(user) {
+  return {
+    type: userActions.FETCH_USER_DATA,
+    user
+  };
+}
+
+export function fetchUserSuccess(user) {
+  return {
+    type: userActions.FETCH_USER_DATA_SUCCESS,
+    user
+  };
+}
+
+export function fetchUserError(message) {
+  return {
+    type: userActions.FETCH_USER_DATA_ERRORS,
     message
   };
 }
