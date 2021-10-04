d92323596443900579d16db5ed93fb71f66a0903
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-10-05 00:18:56 +0200

    add type in lastfmReducer

--- src/reducers/lastFmReducer.js
+++ src/reducers/lastFmReducer.js
@@ -1,19 +1,24 @@
 import { lastFmActions } from "../constants";
 
+interface LastFmReducerInterface {
+  data: Array<any> | null;
+  error: string | null;
+}
+
 const initialState = {
   data: [],
   error: null
 }
 
-export default function(state = initialState, action) {
+export default function(state: LastFmReducerInterface = initialState, action: any) {
   const { type } = action;
   switch(type) {
     case lastFmActions.FETCH_LAST_FM_SUCCESS:
       const { from, to, payload} = action;
-      const newChunk = { payload, from, to};
+      const newChunk = { payload, from, to };
       return { data: [...state.data, newChunk], error: null }
     case lastFmActions.FETCH_LAST_FM_ERRORS:
-      return Object.assign({}, state, { error: action.message });
+      return { ...state, error: action.message };
     default:
       return state;
   }
