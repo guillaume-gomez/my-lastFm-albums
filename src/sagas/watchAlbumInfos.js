70fbf58d44482b9275d3502df18e33a334796685
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-10-08 14:58:06 +0200

    fix how the api call is done to make sure everything we gave an answer at the related reducer

--- src/sagas/watchAlbumInfos.js
+++ src/sagas/watchAlbumInfos.js
@@ -5,16 +5,24 @@ import { fetchAlbumInfosSuccess, fetchAlbumInfosError} from '../actions/lastFmAc
 const api_key = process.env.REACT_APP_API_KEY;
 
 export function* lastFmAlbumInfosQuery(params) {
-    const { user, album} = params;
-    let url = `https://ws.audioscrobbler.com/2.0/?method=album.getInfo&user=${user}&api_key=${api_key}&artist=${album.artist["#text"]}&album=${album.name}&format=json`;
-    const response = yield fetch(url, {
-      method: 'get',
-    })
-    .then((res) => res.json());
-    const { error } = response;
-    if(error) {
-      yield put(fetchAlbumInfosError(response.message));
-    } else {
-      yield put(fetchAlbumInfosSuccess(response.album));
+    try {
+      const { user, album } = params;
+      const userEncoded = encodeURIComponent(user);
+      const artistEncoded = encodeURIComponent(album.artist["#text"]);
+      const nameEncoded = encodeURIComponent(album.name);
+      
+      let url = `https://ws.audioscrobbler.com/2.0/?method=album.getInfo&user=${userEncoded}&api_key=${api_key}&artist=${artistEncoded}&album=${nameEncoded}&format=json`;
+      const response = yield fetch(url, {
+        method: 'get',
+      })
+      .then((res) => res.json());
+      const { error } = response;
+      if(error) {
+        yield put(fetchAlbumInfosError(response.message));
+      } else {
+        yield put(fetchAlbumInfosSuccess(response.album));
+      }
+    } catch(e) {
+      yield put(fetchAlbumInfosError(e.message));
     }
 }
