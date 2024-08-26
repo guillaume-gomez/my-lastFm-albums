0dfe9321bea9db290e84db63711e093b727f8933
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-08-27 00:05:03 +0200

    update theme

--- src/main.tsx
+++ src/main.tsx
@@ -12,10 +12,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {ThemeProvider } from "@mui/material";
-import createTheme from "@mui/material/styles/createTheme";
+import { ThemeProvider } from "@mui/material";
 
-const theme = createTheme({});
+import theme from "./theme";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+import createTheme from "@mui/material/styles/createTheme";
+
+const theme = createTheme({
+   palette: {
+    mode: "dark",
+    primary: {
+      main: '#8e0000',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+});
+
+export default theme;
