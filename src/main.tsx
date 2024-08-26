d1e6cb13b4201564e2de4c13239d333cae9ba2f6
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-08-27 00:11:08 +0200

    add cssbaseline to convert the style properly

--- src/main.tsx
+++ src/main.tsx
@@ -14,12 +14,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { ThemeProvider } from "@mui/material";
 
+import CssBaseline from '@mui/material/CssBaseline';
+
 import theme from "./theme";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
+        <CssBaseline />
         <App />
       </Provider>
     </ThemeProvider>
